bda8e9f4a0b16a3ba0d278654cc0aaaf6b8e3645
pawelmhm <pawel@zyte.com>
2024-08-31 19:12:00 +0200

    mess

---
+++ main.py
@@ -0,0 +1,22 @@
+from socketify import App
+import ujson
+import time
+
+app = App()
+app.json_serializer(ujson)
+router = app.router()
+
+@router.get("/")
+def home(res, req):
+    print("request made!")
+    time.sleep(1)
+    res.end({"a":"Hello World!"})
+
+@router.get("/hello")
+def hello(res, req):
+
+   res.end("Hello API!")
+
+if __name__ == "__main__":
+    app.listen(3000, lambda config: print("Listening on port http://localhost:%d now\n" % (config.port)))
+    app.run()
